9d19c0cf8b826fa31896e2e55778f661f7994fdf
B-rimel <luca.afev78@gmail.com>
2023-10-20 00:59:52 +0200

    Ajout d'animations et de transitions

---
+++ index.js
@@ -0,0 +1,18 @@
+// function addElementWithDelay(tag, text, delay) {
+//   const element = document.createElement(tag);
+//   element.textContent = text;
+//   element.style.opacity = 0;
+//   element.style.transform = "translateY(20px)";
+//   setTimeout(() => {
+//     element.style.opacity = 1;
+//     element.style.transform = "translateY(0)";
+//   }, delay);
+//   setTimeout(() => {
+//     document.getElementById("hero-title").appendChild(element);
+//   }, delay);
+// }
+
+// // Ajoutez chaque élément avec un délai de 0.3 secondes entre eux
+// addElementWithDelay("h1", "Luca Campagna - Développeur Web full-stack", 300);
+// addElementWithDelay("h1", "Vous avez une vision. J'ai les outils.", 600);
+// addElementWithDelay("button", "On collabore ?", 900);
