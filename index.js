447ca70bc7715fffe3ad24d9a7fcf968b4213577
b-rimel <luca.afev78@gmail.com>
2023-12-14 17:15:49 +0100

    Réglage du CSS, implémentation de l'appel API

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const formData = document.getElementById("form");
 const inputName = document.getElementById("name");
 const inputEmail = document.getElementById("email");
 const inputContent = document.getElementById("content");
-
+const inputSend = document.getElementById("submit");
 const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-z]{2,}$/;
 
 function isEmail() {
   return regex.test(inputEmail.content);
 }
-inputEmail.addEventListener("change", isEmail);
-const validEmail = isEmail(inputEmail);
 
-formData.addEventListener("submit", (event) => {
-  event.preventDefault;
+formData.addEventListener("submit", async (event) => {
+  event.preventDefault();
   if ((inputName, inputEmail, inputContent && isEmail)) {
-    const email = new FormData();
-    email.append("name", inputName.content);
-    email.append("email", inputEmail.content);
-    email.append("content", inputContent.content);
-    fetch(`lucacampagna.alwaysdata.net/mail`, {
+    const email = {
+      name: inputName.value,
+      email: inputEmail.value,
+      content: inputContent.value,
+    };
+    console.log(email);
+    fetch(`localhost:3000`, {
       method: "POST",
       body: email,
     }).then(formData.reset());
